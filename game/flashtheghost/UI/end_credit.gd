8aea06be178f4e7882f84e44c4c58b462618d517
StevenAlfon <saayojoshua@gmail.com>
2026-01-03 11:27:56 +0800

    added cursor on the end

--- game/flashtheghost/UI/end_credit.gd
+++ game/flashtheghost/UI/end_credit.gd
@@ -2,6 +2,9 @@ extends Control
 
 @onready var end_credit: Control = $end_credit
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _start_credits() -> void:
 	end_credit.visible = false
 	await get_tree().create_timer(3.0).timeout
